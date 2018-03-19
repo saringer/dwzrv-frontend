f5b714c1a5686db0bb51bdbb1747650714084128
saringer <bidding@web.de>
2018-03-19 13:03:07 +0100

    update

--- AngularPrototype/src/app/appsettings.ts
+++ AngularPrototype/src/app/appsettings.ts
@@ -1,7 +1,7 @@
 export class AppSettings {
 
- // public static server: string = '83.169.5.181:';
-  public static server: string = 'http://localhost:';
+  public static server: string = '83.169.5.181:';
+  //public static server: string = 'http://localhost:';
 
 
   // TournamentDog
